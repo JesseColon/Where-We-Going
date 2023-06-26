af047e033144a7e90c1fb4a37a610b896d6205b7
JesseColon <jay.alexandervii@gmail.com>
2023-06-26 19:24:46 -0400

    added connections

--- Config/connection.js
+++ Config/connection.js
@@ -1,10 +1,20 @@
 const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/config.json')[env];
-
-const connection = new Sequelize(config.database, config.username, config.password, {
-    host: config.host,
-    dialect: config.dialect,
-});
-
-module.exports = connection;
+let sequelize;
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+  } else {
+    sequelize = new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+      }
+    );
+  }
+ 
+  module.exports = sequelize;
