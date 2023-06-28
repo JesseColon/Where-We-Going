09e92def8ccdb6ed76c720447b6160842d568c9d
JesseColon <jay.alexandervii@gmail.com>
2023-06-28 19:12:53 -0400

    added utils/ fix server issue

--- models/event.js
+++ models/event.js
@@ -1,5 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('./config/connection');
+const sequelize = require('../Config/connection');
 
 class Event extends Model {}
 
