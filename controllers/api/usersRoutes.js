12f1a71a83215626323b94448e2cd367746f2fe5
Dominick-D <127568682+Dominick-D@users.noreply.github.com>
2023-07-06 17:58:36 -0400

    asdf

--- controllers/api/usersRoutes.js
+++ controllers/api/usersRoutes.js
@@ -11,7 +11,7 @@ router.post('/', async (req, res) => {
         req.session.save(() => {
             req.session.user_id = userData.id;
             req.session.logged_in = true;
-            res.redirect('/login');
+            res.redirect('/dashboard');
         });
     } catch (err) {
         res.status(400).json(err);
@@ -37,10 +37,11 @@ router.post('/login', async (req, res) => {
         req.session.save(err => {
             if (err) {
                 return res.status(500).json(err);
-            }
+            } else {
             // Redirect the user to the dashboard page
             res.redirect('/dashboard');
-        });
+        }});
+    
     } catch (err) {
         res.status(400).json(err);
     }
@@ -77,10 +78,10 @@ router.get('/users', (req, res) => {
 });
 
  // Route to create a new user
- router.post('/users', (req, res) => {
-     db.User.create(req.body)
-         .then(newUser => res.json(newUser))
-         .catch(err => res.status(500).json(err));
- });
+//  router.post('/users', (req, res) => {
+//      db.User.create(req.body)
+//          .then(newUser => res.json(newUser))
+//          .catch(err => res.status(500).json(err));
+//  });
 
 module.exports = router;
