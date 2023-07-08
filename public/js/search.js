6be61eaa8e1aecd2293b47186a5d5f1a32a22124
Dominick-D <127568682+Dominick-D@users.noreply.github.com>
2023-07-08 00:33:06 -0400

    search starting to work

--- public/js/search.js
+++ public/js/search.js
@@ -1,33 +1,16 @@
 const searchButton = document.getElementById("searchButton");
 const searchInput = document.getElementById("searchInput");
 const searchCategory = document.getElementById("searchCategory");
-const searchZip = document.getElementById("searchZip");
 const resultsSection = document.getElementById("resultsSection");
 
 const locationType = document.getElementById("locationType");
 const locationInputField = document.getElementById("locationInputField");
-const stateCodeInputField = document.getElementById("stateCodeInputField"); // Add this line
-
-locationType.addEventListener("change", () => {
-    if (locationType.value) {
-        locationInputField.style.display = "block";
-        if (locationType.value === "stateCode") {
-            stateCodeInputField.style.display = "block"; 
-            locationInputField.style.display = "none";
-        } else {
-            stateCodeInputField.style.display = "none"; 
-            locationInputField.style.display = "block";
-        }
-    } else {
-        locationInputField.style.display = "none";
-        stateCodeInputField.style.display = "none"; 
-    }
-});
+const stateCodeInputField = document.getElementById("stateCodeInputField");
+const stateCodeInput = document.getElementById("stateCodeInput");
 
 fetch('../states.json')
     .then(response => response.json())
     .then(states => {
-        const stateCodeInput = document.getElementById('stateCodeInput');
         for (const code in states) {
             const option = document.createElement('option');
             option.value = code;
@@ -36,45 +19,61 @@ fetch('../states.json')
         }
     });
 
-    searchButton.addEventListener("click", async () => {
-        const query = searchInput.value;
-        const category = searchCategory.value;
-        const zip = searchZip.value;
-    
-        let locationValue;
+locationType.addEventListener("change", () => {
+    if (locationType.value) {
         if (locationType.value === "stateCode") {
-            const stateCodeInput = document.getElementById("stateCodeInput");
-            locationValue = stateCodeInput ? stateCodeInput.value : '';
+            stateCodeInputField.style.display = "block";
+            locationInputField.style.display = "none";
         } else {
-            const locationInput = document.getElementById("locationInput");
-            locationValue = locationInput ? locationInput.value : '';
+            stateCodeInputField.style.display = "none";
+            locationInputField.style.display = "block";
         }
-    
-        try {
-            const response = await fetch(`/api/search?keyword=${query}&segmentName=${category}&${locationType.value}=${locationValue}`);
-            const data = await response.json();
-    
-            // Clear previous results
-            resultsSection.innerHTML = '';
-    
-            // Check if data._embedded exists before trying to access data._embedded.events
-            if (data._embedded) {
-                // Loop through the events and append them to the results section
-                data._embedded.events.forEach(event => {
-                    const eventDiv = document.createElement('div');
-                    eventDiv.innerHTML = `
-                        <h2>${event.name}</h2>
-                        <p>${event.dates.start.localDate}</p>
-                        <a href="${event.url}" target="_blank">More Info</a>
-                    `;
-                    resultsSection.appendChild(eventDiv);
-                });
-            } else {
-                resultsSection.innerHTML = '<p>No events found for the given search parameters.</p>';
-            }
-        } catch (error) {
-            console.error(error);
+    } else {
+        locationInputField.style.display = "none";
+        stateCodeInputField.style.display = "none";
+    }
+});
+
+searchButton.addEventListener("click", async () => {
+    const query = searchInput.value;
+    const category = searchCategory.value;
+
+    let locationValue;
+    if (locationType.value === "stateCode") {
+        locationValue = stateCodeInput.value;
+    } else {
+        locationValue = locationInputField.value;
+    }
+
+    let locationParam = '';
+    if (locationType.value) {
+        locationParam = `${locationType.value}=${locationValue}`;
+    }
+
+    try {
+        const response = await fetch(`/api/search?keyword=${query}&segmentName=${category}&${locationParam}`);
+        const data = await response.json();
+
+        // Clear previous results
+        resultsSection.innerHTML = '';
+
+        // Check if data._embedded exists before trying to access data._embedded.events
+        if (data._embedded) {
+            // Loop through the events and append them to the results section
+            data._embedded.events.forEach(event => {
+                const eventDiv = document.createElement('div');
+                eventDiv.innerHTML = `
+                    <h2>${event.name}</h2>
+                    <p>${event.dates.start.localDate}</p>
+                    <a href="${event.url}" target="_blank">More Info</a>
+                `;
+                resultsSection.appendChild(eventDiv);
+            });
+        } else {
+            resultsSection.innerHTML = '<p>No events found for the given search parameters.</p>';
         }
-    });
-    
-    
+    } catch (error) {
+        console.error(error);
+    }
+});
+
