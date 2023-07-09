7e5fbbe10a8a0534de44b3fa5c8bec84360cb35c
Dominick-D <127568682+Dominick-D@users.noreply.github.com>
2023-07-09 16:51:03 -0400

    7/9/2023 4:50pm
    small changes

--- controllers/api/searchRoutes.js
+++ controllers/api/searchRoutes.js
@@ -5,18 +5,27 @@ const fetch = require('node-fetch');
 const APIKEY = process.env.API_KEY; 
 
 router.get('/', async (req, res) => {
-    const { keyword, segmentName, postalCode, city, countryCode, stateCode } = req.query;
+    const { keyword, segmentName, postalCode, city, countryCode, stateCode, radius } = req.query;
 
     let locationParam;
+    let radiusParam = '';
     if (postalCode) {
         locationParam = `postalCode=${postalCode}`;
+        if (radius) {
+            radiusParam = `&radius=${radius}&unit=miles`;
+        }
     } else if (city) {
         locationParam = `city=${city}`;
+        if (radius) {
+            radiusParam = `&radius=${radius}&unit=miles`;
+        }
     } else if (countryCode) {
         locationParam = `countryCode=${countryCode}`;
     } else if (stateCode) {
         locationParam = `stateCode=${stateCode}`;
     }
+
+    
 
     const url = `https://app.ticketmaster.com/discovery/v2/events.json?keyword=${keyword}&classificationId=${segmentName}&${locationParam}&apikey=${APIKEY}`;
 
